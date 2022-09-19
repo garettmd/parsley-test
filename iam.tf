cfee623ad3d50b6e3469f3c30a5dde745c28e1d3
garettmd <resume@garettmd.com>
2022-09-18 22:50:06 -0400

    missing statement

--- iam.tf
+++ iam.tf
@@ -73,6 +73,7 @@ resource "aws_iam_role_policy_attachment" "developer_role_dyanmo_rw" {
 }
 
 data "aws_iam_policy_document" "dynamodb_parsley_ro" {
+  statement {
   sid    = "ReadOnlyAPIActionsOnBooks"
   effect = "Allow"
   actions = [
@@ -85,6 +86,7 @@ data "aws_iam_policy_document" "dynamodb_parsley_ro" {
   resources = [
     aws_dynamodb_table.parsley.arn
   ]
+}
 }
 
 resource "aws_iam_policy" "dynamodb_parsley_ro" {
