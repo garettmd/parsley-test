60820bcb84f33c14a8e3ac68515c72130f964518
garettmd <resume@garettmd.com>
2022-09-18 23:18:15 -0400

    added s3 backend

--- provider.tf
+++ provider.tf
@@ -4,4 +4,12 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.1.8"
+
+  # For some reason, TF isn't able to find this bucket created with awslocal, and while running tflocal. But this is how the configuration should work.
+  backend "s3" {
+    bucket = "parsley-tf-state"
+    key    = "test"
+    region = "us-east-1"
+  }
 }
+
