f92d926cdff05a583ebadc51d00dde9738eea5aa
garettmd <resume@garettmd.com>
2022-09-18 22:50:42 -0400

    made range key and changed billing mode for simplicity

--- dynamo.tf
+++ dynamo.tf
@@ -1,6 +1,8 @@
 resource "aws_dynamodb_table" "parsley" {
-  name     = "parsley"
-  hash_key = "Parsley-1"
+  name         = "parsley"
+  hash_key     = "Parsley-1"
+  range_key    = "Parsley-2"
+  billing_mode = "PAY_PER_REQUEST"
 
   attribute {
     name = "Parsley-1"
